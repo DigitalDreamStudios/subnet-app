2462377ff968ab1a3069530dd5007a88438b3919
Diana-Raquel <00096520@uca.edu.sv>
2023-06-04 20:07:24 -0600

    Add: Header

--- app/src/main/java/com/evildev/subnetapp/MainActivity.kt
+++ app/src/main/java/com/evildev/subnetapp/MainActivity.kt
@@ -3,14 +3,15 @@ package com.evildev.subnetapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.evildev.subnetapp.ui.SubnetApp
+import com.evildev.subnetapp.ui.components.SubneteoHeader
 import com.evildev.subnetapp.ui.theme.SubnetAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +23,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Tittle()
                     SubnetApp()
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Tittle() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        SubneteoHeader()
     }
 }
 
--- app/src/main/java/com/evildev/subnetapp/ui/components/HeaderSub.kt
+++ app/src/main/java/com/evildev/subnetapp/ui/components/HeaderSub.kt
@@ -1,2 +1,64 @@
 package com.evildev.subnetapp.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SubneteoHeader() {
+
+
+    Scaffold(
+        modifier = Modifier.background(Color(0xFF1E1E1E)),
+        topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(
+                            "Subnet App",
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    }
+                )
+        },
+        content = { innerPadding ->
+            LazyColumn(
+                contentPadding = innerPadding,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+
+            }
+        }
+    )
+}
+
+
+@Preview
+@Composable
+fun PreviewSubneteoHeader() {
+    SubneteoHeader()
+}
